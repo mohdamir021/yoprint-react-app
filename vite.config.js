62b065b37be422cb002fde47c88b9e82b9d83914
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-11-01 19:55:48 +0800

    feat: configure chakra ui and typescript  with vite

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
-    port: 4000,
+    port: 4000, // Set up the development server to run on port 4000
   }
 })
