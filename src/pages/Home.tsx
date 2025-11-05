638c656b9d03a9c5b91c094c041b8ee8f41d152e
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-11-05 22:02:42 +0800

    feat(Home): update css props on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,28 @@
-import { Box, Wrap } from "@chakra-ui/react";
+import { Box, Stack, Wrap } from "@chakra-ui/react";
 import React from "react";
 import AnimeCard from "../components/main/AnimeCard";
 import { generateArrayOfNumbers } from "../utils/helpers";
 
 export default function Home() {
   return (
-    <Box>
-      <Wrap justify={"center"} gap={6}>
-        {generateArrayOfNumbers(9).map((num)=>(<AnimeCard key={`${num}_anime-card`} />))}
+    <Stack
+      w={"full"}
+      px={"10px"}
+      alignItems={"center"}
+      // Outline
+      // bgColor={"red"}
+    >
+      <Wrap
+        maxW={"800px"}
+        justifyContent={"center"}
+        gap={6}
+        // Outline
+        // bg={"yellow"}
+      >
+        {generateArrayOfNumbers(9).map((num) => (
+          <AnimeCard key={`${num}_anime-card`} />
+        ))}
       </Wrap>
-    </Box>
+    </Stack>
   );
 }
