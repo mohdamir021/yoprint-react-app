c4d4f9f9283b0ac32f197ff9aa9c85e5114a95f7
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-11-05 22:16:50 +0800

    wip(List): add anime item list component

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { Box, Flex, Stack, VStack } from "@chakra-ui/react";
+import React from "react";
+import { generateArrayOfNumbers } from "../utils/helpers";
+import AnimeItemList from "../components/main/AnimeItemList";
 
 export default function List() {
   return (
-    <div>List</div>
-  )
+    <VStack w={"full"} gap={4}>
+      {generateArrayOfNumbers(5).map((value) => (
+        <AnimeItemList key={`${value}-anime-item-list`} />
+      ))}
+    </VStack>
+  );
 }
