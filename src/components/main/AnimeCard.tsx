6fed52a06593361af056cd50d6afb979ce043d01
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-11-09 12:40:08 +0800

    wip: create show page link

--- src/components/main/AnimeCard.tsx
+++ src/components/main/AnimeCard.tsx
@@ -1,4 +1,5 @@
 import { Box, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import React from "react";
 
 export default function AnimeCard() {
@@ -15,9 +16,11 @@ export default function AnimeCard() {
       border={"1px solid red"}
     >
       {/* Outline */}
-      <Box w={"160px"} bgColor={"blue"} h={"200px"}/>
+      <Box w={"160px"} bgColor={"blue"} h={"200px"} />
 
-      <Text>Anime Name</Text>
+      <Link to={"/show/anime-id"}>
+        <Text _hover={{color: "orange", fontWeight: "bold"}}>Anime Name</Text>
+      </Link>
     </VStack>
   );
 }
--- src/pages/Show.tsx
+++ src/pages/Show.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { Box, Flex, Heading, HStack, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+import { lorem } from "../libs/lorem";
 
 export default function Show() {
+  // consoles
+  console.log("Show");
+
   return (
-    <div>Show</div>
-  )
+    <VStack w={"full"} h={"full"} px={"6"} py={"4"}>
+
+      {/* Upper Part */}
+      <Heading size={"3xl"}>Anime Name</Heading>
+
+      <HStack align={"start"} w={"full"} maxW={"900px"} gap={"30px"}>
+        <Box
+          minW={"260px"}
+          minH={"320px"}
+          // outline
+          border={"1px solid yellow"}
+        >
+          <Text>Picture</Text>
+
+        </Box>
+
+        <Stack
+          h={"full"}
+          w={"full"}
+          p={2}
+          // Outline
+          border={"1px solid orange"}
+        >
+          <Text>{lorem.generateParagraphs(1)}</Text>
+          <Text>{lorem.generateParagraphs(1)}</Text>
+          <Text>{lorem.generateParagraphs(1)}</Text>
+        </Stack>
+      </HStack>
+    </VStack>
+  );
 }
