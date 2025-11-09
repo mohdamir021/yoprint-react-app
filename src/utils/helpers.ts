8a3e42d62c6304c78c831dcb353e3bb2f4ad7973
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-11-09 14:05:56 +0800

    feat(helpers): add catchAndLogError function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,3 +4,7 @@ export function generateArrayOfNumbers(length:number, multiplesOf?: number) {
   const multiples = multiplesOf ?? 1;
   return Array.from({length}, (_,index) => index * multiples)
 }
+
+export function catchAndLogError (error: unknown) {
+  console.log(error)
+}
