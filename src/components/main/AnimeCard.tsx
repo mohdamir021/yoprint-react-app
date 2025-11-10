d36e1a4568a196b59dd2d467bb7236142aa50919
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-11-10 17:41:41 +0800

    wip(AnimeCard): progress to adding integration

--- src/components/main/AnimeCard.tsx
+++ src/components/main/AnimeCard.tsx
@@ -1,25 +1,31 @@
-import { Box, Text, VStack } from "@chakra-ui/react";
+import { Box, Image, Text, VStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import React from "react";
+import { AnimeDetails } from "../../interfaces/search";
 
-export default function AnimeCard() {
+export default function AnimeCard(props: Partial<AnimeDetails>) {
+
+  const {title, images} = props;
+
   return (
     <VStack
       w={"full"}
       maxW={"180px"}
-      h={"260px"}
+      h={"280px"}
       p={1}
       rounded={"5px"}
       borderRadius={"5px"}
-      justifyContent={"center"}
+      justify={"start"}
+      align={"center"}
+      textAlign={"center"}
       // Outline
       border={"1px solid red"}
     >
       {/* Outline */}
-      <Box w={"160px"} bgColor={"blue"} h={"200px"} />
+      {/* <Box w={"160px"} bgColor={"blue"} h={"200px"} /> */}
+      <Image alt={title} src={images?.jpg.image_url} w={"160px"} h={"200px"} />
 
       <Link to={"/show/anime-id"}>
-        <Text _hover={{color: "orange", fontWeight: "bold"}}>Anime Name</Text>
+        <Text fontSize={"14px"} _hover={{color: "orange", fontWeight: "bold"}}>{title}</Text>
       </Link>
     </VStack>
   );
