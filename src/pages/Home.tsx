1d428f21154e437b8146a7369d4a0397e3e63ff3
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-11-10 17:43:43 +0800

    wip(Home): progress ro api integration

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,33 @@
-import { Box, Stack, Text, Wrap } from "@chakra-ui/react";
+import { Box, Pagination, Stack, Text, Wrap } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import AnimeCard from "../components/main/AnimeCard";
 import { catchAndLogError, generateArrayOfNumbers } from "../utils/helpers";
 import { animeService } from "../services";
 import { AnimeDetails } from "../interfaces/search";
+import SearchPagination from "../components/main/SearchPagination";
+import useStatuses from "../hooks/useStatuses";
 
 export default function Home() {
+  const { isSuccess, isLoading, isError, isEmpty, setSuccess, setEmpty, setLoading, catchError } =
+    useStatuses();
   const [list, setList] = useState<AnimeDetails[]>([]);
 
   useEffect(() => {
-    if(list.length === 0) {
+    if (isLoading && list.length === 0) {
       animeService
-        .index({ page: 1, limit: 10, order_by: "start_date", sort: "desc" })
+        .index({ page: 1, limit: 12, order_by: "start_date", sort: "desc" })
         .then((response) => {
           setList(response?.data ?? []);
+
+          // Status Update
+          setSuccess(true);
+          setLoading(false);
+          setEmpty(response?.data.length === 0)
+          
         })
-        .catch(catchAndLogError);
+        .catch(catchError);
     }
-  }, [list]);
-
-  console.log(list)
-
-  // flags
-  const isEmpty = list?.length === 0;
+  }, [list, isSuccess, isLoading]);
 
   return (
     <Stack
@@ -36,15 +41,21 @@ export default function Home() {
         maxW={"800px"}
         justifyContent={"center"}
         gap={6}
+        mb={6}
         // Outline
         // bg={"yellow"}
       >
-        {!isEmpty ? (
-          list.map((num) => <AnimeCard key={`${num}_anime-card`} />)
-        ) : (
-          <Text>No Anime Found</Text>
-        )}
+        {isLoading && <Text>Is Loading</Text>}
+        {isError && <Text>Error</Text>}
+        {isSuccess &&
+          !isEmpty &&
+          list.map((data, index) => (
+            <AnimeCard key={`${index}_${data.mal_id}_anime-card`} {...data} />
+          ))}
+        {isEmpty && <Text>No Anime Found</Text>}
       </Wrap>
+      {/* Pagination */}
+      {!isEmpty && <SearchPagination />}
     </Stack>
   );
 }
