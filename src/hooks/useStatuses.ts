78f1afcf11ce788946d328f6c68a04dbb14338b4
mohdamir021 <amir.ibrahim201200@gmail.com>
2025-11-11 23:12:12 +0800

    feat(useStatuses): formatting and added isRefetching

--- src/hooks/useStatuses.ts
+++ src/hooks/useStatuses.ts
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 interface StatusType {
-  isSuccess: boolean
-  isLoading: boolean
-  isError: boolean
-  isEmpty: boolean
+  isSuccess: boolean;
+  isLoading: boolean;
+  isError: boolean;
+  isEmpty: boolean;
+  isReFetching: boolean;
 }
-
 
 export default function useStatuses(initialValues?: Partial<StatusType>) {
   const [status, setStatuses] = useState<StatusType>({
@@ -14,27 +14,31 @@ export default function useStatuses(initialValues?: Partial<StatusType>) {
     isLoading: true,
     isError: false,
     isEmpty: false,
+    isReFetching: false,
     ...initialValues,
   });
 
   return {
     ...status,
     // methods
-    setSuccess (isSuccess: boolean) {
-      setStatuses(prev => ({...prev, isSuccess}))
+    setSuccess(isSuccess: boolean) {
+      setStatuses((prev) => ({ ...prev, isSuccess }));
     },
-    setEmpty (isEmpty: boolean) {
-      setStatuses(prev => ({...prev, isEmpty}))
+    setEmpty(isEmpty: boolean) {
+      setStatuses((prev) => ({ ...prev, isEmpty }));
     },
-    setLoading (isLoading: boolean) {
-      setStatuses(prev => ({...prev, isLoading}))
+    setLoading(isLoading: boolean) {
+      setStatuses((prev) => ({ ...prev, isLoading }));
     },
-    setError (isError: boolean) {
-      setStatuses(prev => ({...prev, isError}))
+    setError(isError: boolean) {
+      setStatuses((prev) => ({ ...prev, isError }));
+    },
+    setRefetching(isReFetching: boolean) {
+      setStatuses((prev) => ({ ...prev, isReFetching }));
     },
     catchError(error: unknown) {
       console.log(error);
       this.setError(true);
-    }
-  }
+    },
+  };
 }
